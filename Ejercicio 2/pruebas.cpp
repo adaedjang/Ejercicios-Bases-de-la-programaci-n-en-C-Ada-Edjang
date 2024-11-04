613a53fe7d9fb49eba45bcb69fcd144f90b57b31
adaedjang <aedjamay@myuax.com>
2024-11-04 18:57:39 +0100

    Ejercicio 2

--- Ejercicio 2/pruebas.cpp
+++ Ejercicio 2/pruebas.cpp
@@ -1,5 +1,22 @@
-//
-// Created by 34601 on 04/11/2024.
-//
 
 #include "pruebas.h"
+#include <iostream>
+
+int main() {
+    int edad;
+
+    // Paso 2: Solicita al usuario su edad
+    std::cout << "Por favor ingresa tu edad: ";
+    std::cin >> edad;
+
+    // Paso 3: Verifica si el usuario es mayor de 18
+    if (edad >= 18) {
+        // Paso 4: Imprime un mensaje diciendo que el usuario es mayor de edad
+        std::cout << "Eres mayor de edad.\n";
+    } else {
+        // Imprime un mensaje diciendo que el usuario es menor de edad
+        std::cout << "Eres menor de edad.\n";
+    }
+
+    return 0;
+}
--- Ejercicio 2/pruebas.h
+++ Ejercicio 2/pruebas.h
@@ -1,16 +1,8 @@
-//
-// Created by 34601 on 04/11/2024.
-//
 
 #ifndef PRUEBAS_H
 #define PRUEBAS_H
 
+#include <iostream>
 
+#endif
 
-class pruebas {
-
-};
-
-
-
-#endif //PRUEBAS_H
