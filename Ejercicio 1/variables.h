df1a6904dcd24d2c2d48215bf7072d509ef06338
adaedjang <aedjamay@myuax.com>
2024-11-04 18:48:42 +0100

    Ejercicio 1

--- Ejercicio 1/variables.h
+++ Ejercicio 1/variables.h
@@ -1,16 +1,9 @@
-//
-// Created by 34601 on 04/11/2024.
-//
 
 #ifndef VARIABLES_H
 #define VARIABLES_H
 
+#include <iostream>
+#include <string>
 
+#endif
 
-class variables {
-
-};
-
-
-
-#endif //VARIABLES_H
